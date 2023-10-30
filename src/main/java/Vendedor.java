33edd11944a1a81c649a0b9a919ad0153b34ceaa
azambrao02 <a.zambrano02@ufromail.cl>
2023-10-30 19:30:00 -0300

    agregar clases y sus atributos

--- src/main/java/Vendedor.java
+++ src/main/java/Vendedor.java
@@ -1,5 +1,11 @@
 public class Vendedor {
     private String nombre;
     private String rut;
-    private String ;
+    private String ID;
+
+    public Vendedor(String nombre, String rut, String ID){
+        this.nombre = nombre;
+        this.rut = rut;
+        this.ID = ID;
+    }
 }
